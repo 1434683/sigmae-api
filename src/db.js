0fbd972a6c38bcdcc8c17dae0ecbce45c70315e1
1434683 <14bpmm2ciaadm@gmail.com>
2025-09-25 21:56:57 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-export const pool = new Pool({
+// pegue do Cloud Run → Variáveis de ambiente do sigmae-api
+const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST,   // pode ser IP público ou socket (ver nota abaixo)
   database: process.env.DB_NAME,
-  port: 5432
+  port: process.env.DB_PORT || 5432,
 });
+
+export default pool;
